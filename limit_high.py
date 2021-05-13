6fc8fe25a2f0c0df4878e9a5ce82528a69ddd544
beikon <kabubeikon@gmail.com>
2021-05-14 00:03:56 +0900

    none

--- limit_high.py
+++ limit_high.py
@@ -12,29 +12,31 @@ def main():
     thead = table.find("thead").findAll("tr")
     tbody = table.find("tbody").findAll("tr")
 
+    # 日次別ファイル作成
     file = open('./limit_high/limit_high_' + dt.now().strftime('%Y%m%d') + '.csv', 'w', encoding="utf-8")
     writer = csv.writer(file, lineterminator='\n')
     # ヘッダー
     for index, row in enumerate(thead):
         cell = row.findAll(['th'])
-        writer.writerow([cell[0].get_text(),
-                         cell[1].get_text(),
-                         cell[2].get_text(),
-                         cell[3].get_text(),
-                         cell[4].get_text(),
-                         cell[4].get_text(),
-                         cell[5].get_text()])
+        writer.writerow(["コード",
+                         "市場",
+                         "名称",
+                         "取引値",
+                         "前日比(%)",
+                         "前日比",
+                         "高値"])
     for index, row in enumerate(tbody):
         cell = row.findAll(['td'])
         writer.writerow([cell[0].get_text(),
                          cell[1].get_text(),
                          cell[2].get_text(),
-                         cell[3].get_text(),
                          cell[4].get_text(),
                          cell[5].get_text(),
-                         cell[6].get_text()])
+                         cell[6].get_text(),
+                         cell[7].get_text()])
     file.close()
 
+    # 累積ファイル作成
     file = open('./data/limit_high.csv', 'a', encoding="utf-8")
     writer = csv.writer(file, lineterminator='\n')
     for index, row in enumerate(tbody):
@@ -43,10 +45,10 @@ def main():
                          cell[0].get_text(),
                          cell[1].get_text(),
                          cell[2].get_text(),
-                         cell[3].get_text(),
                          cell[4].get_text(),
                          cell[5].get_text(),
-                         cell[6].get_text()])
+                         cell[6].get_text(),
+                         cell[7].get_text()])
     file.close()
 
 
