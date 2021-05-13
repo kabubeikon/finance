1e6b432ec802b3b21b4406063637311d7aab02a5
beikon <kabubeikon@gmail.com>
2021-05-13 20:32:48 +0900

    none

--- earnings_announcement.py
+++ earnings_announcement.py
@@ -7,38 +7,7 @@ from bs4 import BeautifulSoup
 
 def main():
     url = "https://kabutan.jp/warning/?mode=4_2&market=0&capitalization=-1&stc=&stm=0&page="
-    file = open('./data/earnings_announcement.csv', 'w', encoding="utf-8")
-    writer = csv.writer(file, lineterminator='\n')
-    html_data = requests.get(url + "0")
-    time.sleep(5)
-    soup = BeautifulSoup(html_data.content, "html.parser")
-    table = soup.findAll("table", {"class": "stock_table"})[0]
-
-    writer.writerow(["コード",
-                     "銘柄名",
-                     "市場",
-                     "決算ポイント",
-                     "株価",
-                     "前日比",
-                     "前日比(%)",
-                     "ＰＥＲ",
-                     "ＰＢＲ"])
-    tbody = table.find("tbody").findAll("tr")
-    for index, row in enumerate(tbody):
-        th = row.findAll(['th'])
-        td = row.findAll(['td'])
-        writer.writerow([td[0].get_text().strip(),
-                         th[0].get_text().strip(),
-                         td[1].get_text().strip(),
-                         td[3].get_text().strip(),
-                         td[4].get_text().strip(),
-                         td[6].get_text().strip(),
-                         td[7].get_text().strip(),
-                         td[8].get_text().strip(),
-                         td[9].get_text().strip()])
-    file.close()
-
-    for i in range(10):
+    for i in range(1, 12):
         file = open('./data/earnings_announcement.csv', 'a', encoding="utf-8")
         writer = csv.writer(file, lineterminator='\n')
         html_data = requests.get(url + str(i))
