cac0b1e81597e07dfd45c7eb737b6fb7b167cd49
beikon <kabubeikon@gmail.com>
2021-05-16 14:14:16 +0900

    東証営業時間・休業日一覧コメント追加

--- holiday.py
+++ holiday.py
@@ -6,6 +6,8 @@ def isHoliday(day):
     dayofweek = date.strftime('%a')
     if dayofweek == "Sat" or dayofweek == "Sun":
         return True
+    # 東証営業時間・休業日一覧
+    # https://www.jpx.co.jp/corporate/about-jpx/calendar/index.html
     holidays = [20210101, 2021010, 20210103, 20210111, 20210211, 20210223, 20210320, 20210429,
                 20210503, 20210504, 20210505, 20210722, 20210723,
                 20210808, 20210809, 20210920, 20210923, 20211103,
@@ -13,4 +15,3 @@ def isHoliday(day):
     if day in holidays:
         return True
     return False
-
